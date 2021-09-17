04641e8cb3e79fbd5afb5e42a5b6b8b77a45b38b
fofonotholt <fofo.notholt@gmail.com>
2021-09-16 21:30:35 -0500

    Uploading pin files

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -20,3 +20,10 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
 });
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+//  Add a marker to the map for Los Angeles, California.
+L.circleMarker([34.0522, -118.2437],{
+    radius: 30,
+    color: "black",
+    fillColor: '#ffffa1'
+}).addTo(map);
